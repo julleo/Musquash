310337c7c821b460845940363d4977df5e591ef8
julleo <jumatina09>
2016-06-19 18:45:35 +0200

    FosDmes2

--- src/Fos_UserBundle/Fos_UserBundle.php
+++ src/Fos_UserBundle/Fos_UserBundle.php
@@ -3,7 +3,26 @@
 namespace Fos_UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+* @ORM\Entity
+* @ORM\Table(name="tuto_user")
+*/
 
 class Fos_UserBundle extends Bundle
 {
+	/**
+    * @ORM\Id
+    * @ORM\Column(type="integer")
+    * @ORM\GeneratedValue(strategy="AUTO")
+    */
+   protected $id;
+
+    public function __construct()
+   {
+       //parent::__construct();
+       // your own logic
+   }
 }
